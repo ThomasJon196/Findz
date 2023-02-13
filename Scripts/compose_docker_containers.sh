6f1f9a94f022d634c9bebb04cf2e3d5bdbd1880a
Thomas Jonas <thomas.jonas@inovex.de>
2023-02-13 01:30:59 +0100

    Added docker doc

--- Scripts/compose_docker_containers.sh
+++ Scripts/compose_docker_containers.sh
@@ -1,4 +1,6 @@
 cd ..
 docker-compose up
 
-# Running via docker requires flask to expose port 0.0.0.0 instead of localhost.
+# Running via docker requires 
+# - flask to expose port 0.0.0.0 instead of localhost.
+# - cloudflared forwarding traffic to findz-container instead of localhost (Set in Cloudflare Tunnel dashboard)
