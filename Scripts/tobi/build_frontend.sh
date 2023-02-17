63e96d55946d82684dd90fd4c0230cec0b89b1eb
Polykarp-bit <63054323+Polykarp-bit@users.noreply.github.com>
2023-02-18 00:06:13 +0100

    fixed alot

---
+++ Scripts/tobi/build_frontend.sh
@@ -0,0 +1,12 @@
+# Build current angular html/css/js files
+# Copy files into static & templates folder for flask server
+
+cd ../../frontend
+ng build --configuration production --build-optimizer
+
+cd ..
+rm -r static/*
+mv frontend/dist/my-first-project/* static/
+mv static/index.html templates/
+
+cp webXR/index.html templates/webXR.html
