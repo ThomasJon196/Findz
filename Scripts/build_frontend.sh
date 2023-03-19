db9a4c22e06772efb04f85e65e5098998c1e6243
Thomas Jonas <thomas.jonas@inovex.de>
2023-03-19 13:26:52 +0100

    Added scripts

--- Scripts/build_frontend.sh
+++ Scripts/build_frontend.sh
@@ -8,5 +8,6 @@ cd ..
 rm -r static/*
 mv frontend/dist/my-first-project/* static/
 mv static/index.html templates/
+cp -r images/* static/
 
 cp webXR/index.html templates/webXR.html
--- Scripts/run_and_build.sh
+++ Scripts/run_and_build.sh
@@ -7,6 +7,7 @@ cd ..
 rm -r static/*
 mv frontend/dist/my-first-project/* static/
 mv static/index.html templates/
+cp -r images/* static/
 
 cp webXR/index.html templates/webXR.html
 
